f911a8d2675dbecc2e5049b045104bb1572383d7
Simon Kühling <mail@simonkuehling.de>
2015-04-17 13:55:29 +0200

    release 0.91-rri1.3.0-01

--- src/ArduinoAVR/Repetier/Repetier.h
+++ src/ArduinoAVR/Repetier/Repetier.h
@@ -23,7 +23,7 @@
 #define _REPETIER_H
 
 #define REPETIER_VERSION "0.91"
-#define REPRAPINDUSTRIAL_FIRMWARE_VERSION "0.91-rri1.2.0-01"
+#define REPRAPINDUSTRIAL_FIRMWARE_VERSION "0.91-rri1.3.0-01"
 
 // ##########################################################################################
 // ##                                  Debug configuration                                 ##
